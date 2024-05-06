2976b5792c0df8d1116e3f8109ce37ccb353c9f0
Ivan Dimitrov <ivan@idimitrov.dev>
2024-05-06 23:47:04 +0300

    with overlays

--- flake.nix
+++ flake.nix
@@ -8,13 +8,17 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-      };
-      pnpm = pkgs.nodePackages_latest.pnpm.override rec {
-        version = "8.15.7";
-        src = pkgs.fetchurl {
-          url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-          sha256 = "sha256-UHg90PowOFLeLdFVfNS58Hy1sBgVSm520PQGNdbO4Bk=";
-        };
+        overlays = [
+          (final: prev: {
+            pnpm = prev.nodePackages_latest.pnpm.override rec {
+              version = "8.15.7";
+              src = prev.fetchurl {
+                url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
+                sha256 = "sha256-UHg90PowOFLeLdFVfNS58Hy1sBgVSm520PQGNdbO4Bk=";
+              };
+            };
+          })
+        ];
       };
     in
     {
