b49dfa63e01bd0f63644ced1bde6914cd26783b2
Ivan Dimitrov <ivan@idimitrov.dev>
2024-05-06 23:37:58 +0300

    initial

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      pnpm = pkgs.nodePackages_latest.pnpm.override rec {
+        version = "8.15.7";
+        src = pkgs.fetchurl {
+          url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
+          sha256 = "sha256-UHg90PowOFLeLdFVfNS58Hy1sBgVSm520PQGNdbO4Bk=";
+        };
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodePackages_latest.nodejs
+          pnpm
+        ];
+      };
+    };
+}
